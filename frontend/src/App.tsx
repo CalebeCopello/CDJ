4cbbf9dcabbb42c0d08c88905df24f8f495ed816
Calebe Copello <calebecopello@gmail.com>
2024-10-11 23:11:52 -0300

    add first route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
 function App() {
-
-  return (
-    <>
-      <div>
-        Hello
-      </div>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+      <Routes>
+        <Route 
+        path='/'
+        element={<Home />}
+        />
+      </Routes>
+      </BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
