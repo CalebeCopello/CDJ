37a9831e0a2fa1920c0d375d3e56f2a9375a606e
Calebe Copello <calebecopello@gmail.com>
2024-10-28 16:47:11 -0300

    create user controller, routes, and signup function

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $fields = $request->validate([
+            'username' => 'required|unique:users|min:3|max:20|regex:/^[a-zA-Z0-9_-]+$/',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:4|max:20'
+        ], [
+            'username.required' => 'The username is required.',
+            'username.unique' => 'The Username is already taken. Please choose another one',
+            'username.min' => 'The Username must be at lest 3 characters long',
+            'username.max' => 'The Username must not be longer than 20 characters long',
+            'username.regex' => 'The Username can only contain letters, numbers, hyphens, and underscores',
+            'email.required' => 'The Email is required',
+            'email.unique' => 'The Email is already taken',
+            'password.required' => 'The Password is required',
+            'password.min' => 'The Password must be 4 or more characters long',
+            'password.max' => 'Password must be at most 20 characters long',
+        ]);
+        $user = User::create($fields);
+
+        return $user;
+    }
+
+    public function signin(Request $request)
+    {
+        return 'signin' . $request;
+    }
+
+    public function signout(Request $request)
+    {
+        return 'signout' . $request;
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::get('/', function () {
+    return 'Hello World!';
+});
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/signin', [AuthController::class, 'signin']);
+Route::post('/signout', [AuthController::class, 'signout']);
