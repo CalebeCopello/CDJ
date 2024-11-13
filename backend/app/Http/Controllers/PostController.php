1dc994a3a85e44e41cc6682bf2bafacd0d22382e
Calebe Copello <calebecopello@gmail.com>
2024-11-12 21:17:00 -0300

    add post validation messages in case of error

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -21,14 +21,18 @@ class PostController extends Controller
             'body' => 'required',
             'img' => 'required',
         ], [
+            'slug.required' => 'The slug is required.',
             'slug.unique' => 'The slug must be unique.',
+            'title.required' => 'The title is required.',
             'title.unique' => 'The title must be unique.',
+            'body.required' => 'The body text is required',
+            'img.required' => 'An image is required',
         ]);
 
         $fields = array_merge($fields, [ 'user_id' => $request->user()->id, 'is_published' => false]);
 
         $post = Post::create($fields);
-        
+
         return response()->json($post, 201);
     }
 }
