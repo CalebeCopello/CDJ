4e96d1f5bbbd37a0c35843eaf98eeffa3b477342
Calebe Copello <calebecopello@gmail.com>
2024-11-26 21:23:03 -0300

    add postFormSchema schema

--- frontend/src/libs/schemas.ts
+++ frontend/src/libs/schemas.ts
@@ -24,4 +24,33 @@ const signInFormSchema = z.object({
 	password: z.string().min(4, { message: 'The Password is at least 4 character long' }).max(20, { message: 'The Password is at most 20 character long' }),
 });
 
-export { signUpFormSchema, signInFormSchema };
+const postFormSchema = z.object({
+	title: z.string().min(10, { message: 'The title must have at least 10 characters long' }).max(300, { message: 'The tile must not be longer than 300 characters long' }),
+	slug: z
+		.string()
+		.min(10, { message: 'The slug must have at least 10 characters long' })
+		.max(60, { message: 'The slug must not be longer than 60 characters long' })
+		.refine((s) => !s.includes(' '), { message: 'No spaces are alowed on the slug' }),
+	img: z
+		.custom<FileList>((value) => value instanceof FileList && value.length === 1, {
+			message: 'An image is required',
+		})
+		.refine(
+			(fileList) => {
+				const allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+				const fileName = fileList[0]?.name || '';
+				const fileExtension = fileName.split('.').pop()?.toLowerCase();
+				return fileExtension && allowedExtensions.includes(fileExtension);
+			},
+			{
+				message: 'Only .jpg, .jpeg, .gif, or .png files are allowed',
+			}
+		)
+		.refine((fileList) => fileList[0].size <= 2 * 1024 * 1024, {
+			message: 'The image must be smaller than 2MB',
+		}),
+	body: z.string({ message: 'A body content is required' }).min(200, { message: 'The body of a post must be at least 200 characters long' }).max(50000, { message: 'The body of a post must not be longer than 50,000 characters long' }),
+	tags: z.string().array().nonempty({ message: 'You must chose at least 1 tag' }),
+	published_at: z.date().nullable(),
+});
+export { signUpFormSchema, signInFormSchema, postFormSchema };
