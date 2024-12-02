b446a05a3f0e7725dc30fbfd8c2907d150036a92
Calebe Copello <calebecopello@gmail.com>
2024-12-02 11:13:59 -0300

    change img parameters

--- frontend/src/libs/schemas.ts
+++ frontend/src/libs/schemas.ts
@@ -31,24 +31,7 @@ const postFormSchema = z.object({
 		.min(10, { message: 'The slug must have at least 10 characters long' })
 		.max(60, { message: 'The slug must not be longer than 60 characters long' })
 		.refine((s) => !s.includes(' '), { message: 'No spaces are alowed on the slug' }),
-	img: z
-		.custom<FileList>((value) => value instanceof FileList && value.length === 1, {
-			message: 'An image is required',
-		})
-		.refine(
-			(fileList) => {
-				const allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
-				const fileName = fileList[0]?.name || '';
-				const fileExtension = fileName.split('.').pop()?.toLowerCase();
-				return fileExtension && allowedExtensions.includes(fileExtension);
-			},
-			{
-				message: 'Only .jpg, .jpeg, .gif, or .png files are allowed',
-			}
-		)
-		.refine((fileList) => fileList[0].size <= 2 * 1024 * 1024, {
-			message: 'The image must be smaller than 2MB',
-		}),
+	img: z.string().min(1, { message: 'An image is required' }),
 	body: z.string({ message: 'A body content is required' }).min(200, { message: 'The body of a post must be at least 200 characters long' }).max(50000, { message: 'The body of a post must not be longer than 50,000 characters long' }),
 	tags: z.string().array().nonempty({ message: 'You must chose at least 1 tag' }),
 	published_at: z.date().nullable(),
