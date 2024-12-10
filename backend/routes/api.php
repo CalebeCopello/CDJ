d423a9ab8d05e6f0de07fde08df8386d4769d431
Calebe Copello <calebecopello@gmail.com>
2024-12-09 21:40:50 -0300

    add comments routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostCommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
@@ -8,15 +9,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
+// Test Api Route
 Route::get('/', function () {
     return 'Hello World!';
 });
 
+// User Authentication Routes
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/signin', [AuthController::class, 'signin']);
+
 // Socialite Routes
+// GitHub
 Route::get('/auth/github/redirect', function () {
     return Socialite::driver('github')->stateless()->redirect();
 });
 Route::get('/auth/github/callback', [AuthController::class, 'github']);
+// Google
 Route::get('/auth/google/redirect', function () {
     return Socialite::driver('google')->stateless()->redirect();
 });
@@ -27,18 +35,22 @@ Route::get('/post/get-all', [PostController::class, 'getAllPosts']);
 Route::get('/post/get/{slug}', [PostController::class, 'getPost']);
 
 // Tags Routes
-
 Route::get('/tags/get-all', [TagController::class, 'getTags']);
-// User Routes
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/signin', [AuthController::class, 'signin']);
-//protected routes
+
+// Comments Routes
+Route::get('/comment/get-all', [PostCommentController::class, 'getComments']);
+
+// Protected Routes
 Route::middleware('auth:sanctum')->group(function () {
-    //users
+    // Users
     Route::delete('/signout', [AuthController::class, 'signout']);
     Route::get('/user/signed', [UserController::class, 'signed']);
     Route::get('/user', function (Request $request) {return 'Allowed'.$request->user(); });
-    //post
+    // Post
     Route::post('/post/add', [PostController::class, 'addPost']);
     Route::post('/post/add-image', [PostController::class, 'uploadImage']);
+    
+    // Comment
+    Route::post('/comment/create', [PostCommentController::class, 'createComments']);
+
 });
