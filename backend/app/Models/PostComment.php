31dfab48fd6f8d5c6a5028a7313c2d5054cb073b
Calebe Copello <calebecopello@gmail.com>
2024-12-09 21:14:04 -0300

    create postModel

---
+++ backend/app/Models/PostComment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'post_comments';
+
+    protected $fillable = ['post_id', 'user_id', 'parent_id', 'comment', 'is_visible'];
+
+}
