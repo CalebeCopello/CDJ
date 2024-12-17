a4319295fcd1a4bbcf1d10670eaa6b6770b1742b
Calebe Copello <calebecopello@gmail.com>
2024-12-16 21:13:23 -0300

    change Post component logic

--- frontend/src/screens/Post.tsx
+++ frontend/src/screens/Post.tsx
@@ -1,17 +1,53 @@
-import { Box, Stack } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Box, Container, Stack } from '@mui/material';
 
 import NavBar from '../components/NavBar';
 import PostView from '../components/PostView';
+import CommentSection from '../components/CommentSection';
+
+import { PostType } from '../libs/types';
 
 const Post = () => {
+	const [isPageLoaded, setIsPageLoaded] = useState<boolean>(false);
+	const [isPostFound, setIsPostFound] = useState<boolean>(false);
+	const [postFetched, setPostFetched] = useState<PostType | undefined>();
+
+	const { slug } = useParams();
+	const API_URL: string = import.meta.env.VITE_API_URL;
+
+	useEffect(() => {
+		setIsPageLoaded(() => true);
+		axios
+			.get(`${API_URL}/post/get/${slug}`)
+			.then((res) => {
+				setIsPostFound(() => true);
+				setPostFetched(() => res.data);
+			})
+			.catch((err) => console.error(err));
+	}, []);
 	return (
 		<>
 			<Box>
 				<NavBar />
 			</Box>
-			<Stack component={'main'}>
-				<PostView />
-			</Stack>
+			{isPageLoaded && isPostFound &&  postFetched &&(
+				<Container>
+					<Stack component={'main'}>
+						<PostView post= {postFetched}/>
+					</Stack>
+					<Box>
+						<CommentSection post= {postFetched} />
+					</Box>
+				</Container>
+			)}
+			{isPageLoaded && !isPostFound && (
+				<div>
+					Post not found. <br/> 404
+				</div>
+			)}
 		</>
 	);
 };
