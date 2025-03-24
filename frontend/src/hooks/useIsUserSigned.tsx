d5dcf66bc5f701852b084dab28ee687edc30f728
Calebe Copello <calebecopello@gmail.com>
2025-03-24 14:05:01 -0300

    added userInfo

--- frontend/src/hooks/useIsUserSigned.tsx
+++ frontend/src/hooks/useIsUserSigned.tsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+import { UserInfoType } from '../libs/types';
+
 const useIsUserSigned = () => {
 	const [isUserInfoloaded, setIsUserInfoloaded] = useState<boolean>(false);
 	const [isUserSigned, setIsUserSigned] = useState<boolean>(false);
+	const [userInfo, setUserInfo] = useState<UserInfoType>()
 
 	useEffect(() => {
 		const URL: string = import.meta.env.VITE_API_URL;
@@ -15,7 +18,8 @@ const useIsUserSigned = () => {
 					Authorization: TOKEN,
 				},
 			})
-			.then(() => {
+			.then((res) => {
+				setUserInfo(() => res.data);
 				setIsUserSigned(() => true);
 			})
 			.catch(() => {
@@ -25,7 +29,7 @@ const useIsUserSigned = () => {
 				setIsUserInfoloaded(() => true);
 			});
 	}, []);
-	return { isUserInfoloaded, isUserSigned };
+	return { isUserInfoloaded, isUserSigned, userInfo };
 };
 
 export default useIsUserSigned;
