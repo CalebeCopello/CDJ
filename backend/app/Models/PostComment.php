71e4b8f81a4b848ec3a2a3f4b5cade4dfc69b23f
Calebe Copello <calebecopello@gmail.com>
2025-03-25 17:35:31 -0300

    added relation belongsTo Post and User

--- backend/app/Models/PostComment.php
+++ backend/app/Models/PostComment.php
@@ -13,4 +13,11 @@ class PostComment extends Model
 
     protected $fillable = ['post_id', 'user_id', 'parent_id', 'comment', 'is_visible'];
 
+    public function post() {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
