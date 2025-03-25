4b1a27164443e4f148a9e10b14ec9da37712e847
Calebe Copello <calebecopello@gmail.com>
2025-03-25 17:36:10 -0300

    added relation belongsTo Post and PostComment

--- backend/app/Models/PostLikes.php
+++ backend/app/Models/PostLikes.php
@@ -12,4 +12,12 @@ class PostLikes extends Model
     protected $table = 'post_likes';
 
     protected $fillable = ['user_id', 'comment_id', 'like_value'];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function comment() {
+        return $this->belongsTo(PostComment::class, 'comment_id');
+    }
 }
