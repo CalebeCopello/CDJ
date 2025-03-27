8415d854297321f53bce22d56e165e1cb7a65569
Calebe Copello <calebecopello@gmail.com>
2025-03-27 20:13:57 -0300

    refactor getInfo function

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
         return ['message' => $request->user()];
     }
 
-    public function getInfo(Request $request, string $slug) {
+    public function getInfo(string $slug) {
         $getPost = 3;
         $getLikes = 5;
         $userInfo = User::whereLike('username', $slug)->first();
@@ -22,20 +22,21 @@ class UserController extends Controller
         }
         $commentsInfo = PostComment::where('user_id', $userInfo['id'])->where('is_visible', true)->orderBy('updated_at', 'desc')->take($getPost)->with('post')->get();
         $likesInfo = PostLikes::where('user_id', $userInfo['id'])->orderBy('updated_at', 'desc')->take($getLikes)->with('comment.user', 'comment.post')->get();
+    
+        $returnComment = NULL;
+        $returnLikes = NULL;
         
-        $returnComment = [];
-        $returnLikes = [];
         if ($commentsInfo->isEmpty()) {
-            $returnComment = null;
+            $returnComment = [];
+            foreach($commentsInfo as $value) {
+                $returnComment[] = ['comment' => $value['comment'], 'post' => $value['post']['title'], 'date' => $value['created_at']] ;
+            }
         }
         if ($likesInfo->isEmpty()) {
-            $returnLikes = null;
-        }
-        foreach($commentsInfo as $value) {
-            $returnComment[] = ['comment' => $value['comment'], 'post' => $value['post']['title'], 'date' => $value['created_at']] ;
-        }
-        foreach($likesInfo as $value) {
-            $returnLikes[] = ['value' => $value['like_value'], 'username' => $value['comment']['user']['username'], 'post' => $value['comment']['post']['title'], 'date' => $value['created_at']];
+            $returnLikes = [];
+            foreach($likesInfo as $value) {
+                $returnLikes[] = ['value' => $value['like_value'], 'username' => $value['comment']['user']['username'], 'post' => $value['comment']['post']['title'], 'date' => $value['created_at']];
+            }
         }
 
         return response()->json(['user' => $userInfo, 'comments' => $returnComment, 'likes' => $returnLikes], 200);
