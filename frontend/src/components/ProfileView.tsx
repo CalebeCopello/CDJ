eda12a49b7e6c1646fd62d9e90b0be4ecef3abbc
Calebe Copello <calebecopello@gmail.com>
2025-03-31 19:22:32 -0300

    fixed no comments/reactions message

--- frontend/src/components/ProfileView.tsx
+++ frontend/src/components/ProfileView.tsx
@@ -68,40 +68,45 @@ const ProfileView: React.FC<VireProfileProp> = ({ userInfo, userComments, userLi
 					elevation={1}
 					sx={{ p: mValue / mValue, mb: mValue }}
 				>
-					{userComments
-						? userComments?.map((comment: UserCommentsType, index: number) => (
-								<Box key={index}>
-									{index === 0 ? '' : <Divider sx={{ my: 1 }} />}
-									<Paper
-										elevation={2}
-										sx={{ p: mValue / mValue }}
+					{userComments ? (
+						userComments?.map((comment: UserCommentsType, index: number) => (
+							<Box key={index}>
+								{index === 0 ? '' : <Divider sx={{ my: 1 }} />}
+								<Paper
+									elevation={2}
+									sx={{ p: mValue / mValue }}
+								>
+									<Typography
+										variant='body1'
+										gutterBottom
 									>
-										<Typography
-											variant='body1'
-											gutterBottom
+										@{userInfo?.username} commented:
+									</Typography>
+									<Box sx={{ border: `1px solid ${theme.palette.secondary.main}`, borderRadius: 1, p: 0.5 }}>{comment.comment}</Box>
+									<Typography
+										variant='body2'
+										align='justify'
+										sx={{ mt: mValue / mValue }}
+									>
+										on{' '}
+										<Link
+											component={ReactRouterLink}
+											to={`/post/view/${comment.slug}`}
+											underline='hover'
 										>
-											@{userInfo?.username} commented:
-										</Typography>
-										<Box sx={{ border: `1px solid ${theme.palette.secondary.main}`, borderRadius: 1, p: 0.5 }}>{comment.comment}</Box>
-										<Typography
-											variant='body2'
-											align='justify'
-											sx={{ mt: mValue / mValue }}
-										>
-											on{' '}
-											<Link
-												component={ReactRouterLink}
-												to={`/post/view/${comment.slug}`}
-												underline='hover'
-											>
-												{comment.post}
-											</Link>{' '}
-											at {startDate(comment.date, true, true)}
-										</Typography>
-									</Paper>
-								</Box>
-						  ))
-						: 'no comments'}
+											{comment.post}
+										</Link>{' '}
+										at {startDate(comment.date, true, true)}
+									</Typography>
+								</Paper>
+							</Box>
+						))
+					) : (
+						<Typography
+							variant='body2'
+							align='center'
+						>{`${userInfo.username} has no comments yet :(`}</Typography>
+					)}
 				</Paper>
 				<Typography
 					component={'h2'}
@@ -115,48 +120,54 @@ const ProfileView: React.FC<VireProfileProp> = ({ userInfo, userComments, userLi
 					elevation={1}
 					sx={{ p: mValue / mValue, mb: mValue }}
 				>
-					{userLikes
-						? userLikes.map((like: UserLikesType, index: number) => (
-								<Box key={index}>
-									{index === 0 ? '' : <Divider sx={{ my: mValue / mValue }} />}
-									<Box>
-										<Typography
-											gutterBottom
-											variant='body2'
+					{userLikes ? (
+						userLikes.map((like: UserLikesType, index: number) => (
+							<Box key={index}>
+								{index === 0 ? '' : <Divider sx={{ my: mValue / mValue }} />}
+								<Box>
+									<Typography
+										gutterBottom
+										variant='body2'
+									>
+										@{userInfo?.username}{' '}
+										{like.value === 1 ? (
+											<ThumbUpAlt
+												fontSize='small'
+												sx={{ verticalAlign: 'middle' }}
+											/>
+										) : (
+											<ThumbDownAlt
+												fontSize='small'
+												sx={{ verticalAlign: 'middle' }}
+											/>
+										)}{' '}
+										@
+										<Link
+											component={ReactRouterLink}
+											to={`/user/view/${like.username}`}
+											underline='hover'
 										>
-											@{userInfo?.username}{' '}
-											{like.value === 1 ? (
-												<ThumbUpAlt
-													fontSize='small'
-													sx={{ verticalAlign: 'middle' }}
-												/>
-											) : (
-												<ThumbDownAlt
-													fontSize='small'
-													sx={{ verticalAlign: 'middle' }}
-												/>
-											)}{' '}
-											@<Link
-												component={ReactRouterLink}
-												to={`/user/view/${like.username}`}
-												underline='hover'
-											>
-												{like.username}
-											</Link>{' '}
-											comment on{' '}
-											<Link
-												component={ReactRouterLink}
-												to={`/post/view/${like.slug}`}
-												underline='hover'
-											>
-												{like.post}
-											</Link>{' '}
-											Stardate {startDate(like.date, true, true)}
-										</Typography>
-									</Box>
+											{like.username}
+										</Link>{' '}
+										comment on{' '}
+										<Link
+											component={ReactRouterLink}
+											to={`/post/view/${like.slug}`}
+											underline='hover'
+										>
+											{like.post}
+										</Link>{' '}
+										Stardate {startDate(like.date, true, true)}
+									</Typography>
 								</Box>
-						  ))
-						: 'no likes'}
+							</Box>
+						))
+					) : (
+						<Typography
+							variant='body2'
+							align='center'
+						>{`${userInfo.username} has no reactions yet :~`}</Typography>
+					)}
 				</Paper>
 			</Container>
 		</>
