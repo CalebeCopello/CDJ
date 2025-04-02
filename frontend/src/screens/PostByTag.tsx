27b4367a5f68ce792ec00bde68fd5aeeb6462107
Calebe Copello <calebecopello@gmail.com>
2025-04-01 21:10:07 -0300

    added PostByTag component

---
+++ frontend/src/screens/PostByTag.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import NavBar from '../components/NavBar';
+import { PostType, TagType } from '../libs/types';
+import axios from 'axios';
+import PostListComponent from './PostListComponent';
+
+const PostByTag = () => {
+	const [fetchedPosts, setFetchedPosts] = useState<PostType[] | null>(null);
+	const [fetchedTag, setFetchedTag] = useState<TagType | null>(null);
+	const [isPageLoaded, setIsPageLoaded] = useState<boolean>(false);
+
+	const API_URL: string = import.meta.env.VITE_API_URL;
+	const { slug } = useParams();
+
+	useEffect(() => {
+		setIsPageLoaded(() => true);
+		axios
+			.get(`${API_URL}/post/get/bytag/${slug}`)
+			.then((res) => {
+				setFetchedPosts(() => res.data.posts);
+				setFetchedTag(() => res.data.tag);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, [slug]);
+	
+	return (
+		<>
+			<NavBar />
+			<PostListComponent fetchedPosts={fetchedPosts} isPageLoaded={isPageLoaded} searchTag={fetchedTag?.name} />
+		</>
+	);
+};
+
+export default PostByTag;
